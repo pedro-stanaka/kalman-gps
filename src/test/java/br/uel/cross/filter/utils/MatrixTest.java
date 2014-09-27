ec0fe6a3b1c3de7411faa40e8c15e61fd5ad93a0
Pedro Sena Tanaka <pedro.stanaka@gmail.com>
2014-09-27 15:41:03 -0300

    Cleaning up the tests.

--- src/test/java/br/uel/cross/filter/utils/MatrixTest.java
+++ src/test/java/br/uel/cross/filter/utils/MatrixTest.java
@@ -1,22 +1,16 @@
 package br.uel.cross.filter.utils;
 
-import junit.framework.TestCase;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.text.DecimalFormat;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class MatrixTest {
 
 
     private Matrix foo;
-    private Matrix bar;
-    private Matrix identity;
-    private Matrix copy;
 
     @Before
     public void setUp() throws Exception {
@@ -25,23 +19,18 @@ public class MatrixTest {
                 4.0, 1.0, 7.0, 9.0,
                 0.0, 0.0, -4.0, -4.0,
                 2.3, 3.4, 3.1, 0.0);
-        copy = foo.clone();
-        bar = new Matrix(4,4);
-        identity = new Matrix(4,4);
-        identity.setIdentityMatrix();
     }
 
 
     @Test
     public void testClone() throws Exception {
-        Matrix tmp = foo.clone();
-        assertThat(tmp.getData()[1][1], is(equalTo( foo.getData()[1][1] )));
-        assertThat(tmp.getData()[2][2], is(equalTo( foo.getData()[2][2] )));
+        Matrix clone = foo.clone();
+        assertThat(clone.getData()[1][1], is(equalTo( foo.getData()[1][1] )));
+        assertThat(clone.getData()[2][2], is(equalTo( foo.getData()[2][2] )));
     }
 
     @Test
     public void testInverse() throws Exception {
-
         assertThat(foo.getData(0,0), is(equalTo(-0.2796610169491526)));
     }
 }
